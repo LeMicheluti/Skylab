9bfac748dea29e37ceaab2943663fc47f6083d13
LeMicheluti <lemicheluti@gmail.com>
2019-11-06 06:48:26 -0300

    Configura NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "607d44948a2fbc",
+    pass: "1383c73212fcd9"
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>"
+  }
+};
